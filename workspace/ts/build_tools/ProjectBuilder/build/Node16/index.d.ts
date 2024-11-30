8cec980edd3899b47fb310f900a6bdd0bbb47cb9
ILLIA POLIANSKYI <polianskii.ilia@gmail.com>
2024-11-30 01:53:10 +0100

    project builder tsconfig.CommonJS.json

--- workspace/ts/build_tools/ProjectBuilder/build/Node16/index.d.ts
+++
@@ -1,7 +0,0 @@
-export interface IBuildModes {
-    CommonJS: any;
-    ESNext: any;
-}
-export declare const BuildModesNumber: IBuildModes;
-export declare const BuildModesString: IBuildModes;
-//# sourceMappingURL=index.d.ts.map
